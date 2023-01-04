a7bc0b027b0e2508185b3c5aac50088291edef65
Murial Anindo <murial.anindo@lonett.com>
2023-01-04 13:17:05 -0500

    adding more logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
+// import SearchIcon from "./search.svg";
+import "./App.css";
 
-function App() {
+const API_URL = " ";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchMovies, setSearchMovies] = useState("");
+
+  useEffect(() => {});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movie Land</h1>
     </div>
   );
-}
+};
 
 export default App;
