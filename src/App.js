83e749dea95663c65db0bfaf5cececac8d3c58b4
Murial Anindo <murial.anindo@lonett.com>
2023-01-05 14:05:12 -0500

    added more changes to make it work

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { useState, useEffect } from "react";
+import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
-// import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = " ";
+const API_URL = "https://";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchMovies, setSearchMovies] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(() => {});
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
   return (
     <div className="app">
       <h1>Movie Land</h1>
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
